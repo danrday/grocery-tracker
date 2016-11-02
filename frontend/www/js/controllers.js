3fc6eb5eab912790e03b1a0876a4ab5254721b38
Dan Day <danday@gmail.com>
2016-11-02 11:49:23 -0500

    display category spending as pie graph

--- frontend/www/js/controllers.js
+++ frontend/www/js/controllers.js
@@ -579,6 +579,9 @@ let totalReceiptCost = 0;
 
 let consolidatedCategories = []
 
+//for d3 pie graph
+let dataset = []
+
 
   var current = null;
     var cnt = 0;
@@ -598,6 +601,8 @@ let consolidatedCategories = []
                   numTimes: cnt,
                   totalCost: cost
                 }
+
+                dataset.push({label: current, count: cost})
 
                 consolidatedCategories.push(thisCategory)
 
@@ -632,6 +637,8 @@ let consolidatedCategories = []
 
         consolidatedCategories.push(thisCategory)
 
+        dataset.push({label: current, count: cost})
+
     }
 
     console.log("totalReceiptCost", totalReceiptCost)
@@ -660,6 +667,16 @@ let consolidatedCategories = []
 
 
     $scope.results = consolidatedCategories
+
+
+
+    // let dataset = [
+    //   { label: 'Abulia', count: 10 },
+    //   { label: 'Betelgeuse', count: 20 },
+    //   { label: 'Cantaloupe', count: 30 },
+    //   { label: 'Dijkstra', count: 40 }
+    // ];
+
 
 
 
@@ -697,6 +714,39 @@ let consolidatedCategories = []
     //
     //   }
     // }
+
+
+
+    var width = 250;
+    var height = 250;
+    var radius = Math.min(width, height) / 2;
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20b);
+
+    var svg = d3.select('#chart')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', 'translate(' + (width / 2) +
+        ',' + (height / 2) + ')');
+
+    var arc = d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius);
+
+    var pie = d3.pie()
+      .value(function(d) { return d.count; })
+      .sort(null);
+
+    var path = svg.selectAll('path')
+      .data(pie(dataset))
+      .enter()
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', function(d) {
+        return color(d.data.label);
+      });
 
 
 
